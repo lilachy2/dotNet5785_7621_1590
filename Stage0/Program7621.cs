e4a972a9e9ec1558a674f16a2cd241c702455be9
lilachy2 <lilachy2015@gmail.com>
2024-11-03 15:40:17 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program7621.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
+namespace Stage0;
+
+partial class Program
+{
+     static void Main(string[] args)
+    {
+        Wwlcome7621();
+        Welcom1590();
+        Console.ReadKey();
+    }
+    static partial void Welcom1590();
+    private static void Wwlcome7621()
+    {
+        Console.WriteLine("Enter your name: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine($"{userName}, welcome to my first console application");
+    }
+}
