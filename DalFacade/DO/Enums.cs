089332351158b527651b865023b02d2d51554949
Yalin-Daniel <yalin3242@gmail.com>
2024-11-11 17:42:05 +0200

    Update to step 3

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -1,1 +1,29 @@
 ﻿namespace DO;
+
+// Volunteer
+public enum role
+{
+    Volunteer,
+    Manager
+}
+
+public enum distance_type
+{
+    Aerial_distance,
+    walking_distance,
+    driving_distance,
+    change_distance_type
+}
+
+// Call
+// להחליט ביחד 
+
+// Assignment
+internal enum AssignmentCompletionType
+{
+    TreatedOnTime, // was treated in time
+    VolunteerCancelled, // self cancellation
+    AdminCancelled, // Canceling an administrator
+    Expired // Cancellation has expired
+}
+
