6fbdd42de3961a2b18b14f788fd75a69532e6b4e
Yalin-Daniel <yalin3242@gmail.com>
2024-11-24 15:43:48 +0200

    Stage 2 Chapter 9

--- DalFacade/DO/Exceptions.cs
+++ DalFacade/DO/Exceptions.cs
@@ -1,15 +1,20 @@
 ﻿namespace DO;
 [Serializable]
+public class DalDoesNotExistException : Exception
+{
+    public DalDoesNotExistException(string? message) : base(message) { }
+
+
+}
 public class DalAlreadyExistsException : Exception
 {
-    public DalAlreadyExistsException(string? message) : base(message) 
-    {}
+    public DalAlreadyExistsException(string? message) : base(message) { }
+
 }
 
-[Serializable]
 public class DalDeletionImpossible : Exception
 {
-    public DalDeletionImpossible(string? message) : base(message)
-    { }
-}
+    public DalDeletionImpossible(string? message) : base(message) { }
 
+
+}
