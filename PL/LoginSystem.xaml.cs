1c77b9bc730e74727adf1cf0067a6e38391a9926
Yalin-Daniel <yalin3242@gmail.com>
2025-01-06 09:55:01 +0200

    Login system - work

--- PL/LoginSystem.xaml.cs
+++ PL/LoginSystem.xaml.cs
@@ -1,69 +1,105 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PL.Volunteer;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace PL
 {
-    /// <summary>
-    /// Interaction logic for LoginSystem.xaml
-    /// </summary>
     public partial class LoginSystem : Window
     {
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
-
-        public LoginSystem()
-        {
-            DataContext = this; 
-            InitializeComponent();
-        }
-
-        // מזהה מתנדב
         public int Id { get; set; }
         public string Password { get; set; }
 
-        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        public LoginSystem()
         {
-
-            if (Id == null || Password==null)
-            {
-                MessageBox.Show("Error:Please enter Full details.");
-                return;
-            }
-
-            var volunteer = s_bl.Volunteer.Read(Id);
-            if (volunteer == null)
-            {
-                MessageBox.Show("Error:Volunteer not registered in the system.");
-                return;
-            }
-
-            if (volunteer.Role == BO.Role.Volunteer)
-            {
-                
-
-            }
-            else if (volunteer.Role == BO.Role.Manager)
-            {
-
-
-            }
+            InitializeComponent();
+            DataContext = this; 
 
         }
 
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var volunteer = s_bl.Volunteer.Read(Id);
+                if (Id == 0 || string.IsNullOrEmpty(Password))
+                {
+                    MessageBox.Show("Error: Please enter full details.");
+                    return;
+                }
 
+                if (volunteer == null)
+                {
+                    MessageBox.Show("Error: Volunteer not registered in the system.");
+                    return;
+                }
 
+                if (volunteer.Role == BO.Role.Volunteer)
+                {
+                    VolunteerWindow volunteerWindow = new VolunteerWindow(Id);
+                    volunteerWindow.ShowDialog();
+                }
+                else if (volunteer.Role == BO.Role.Manager)
+                {
+                    var result = MessageBox.Show("Select the screen you want to open:\n" +
+                                                  "1. Volunteer Window\n" +
+                                                  "2. Volunteer in List Window",
+                                                  "Manager Selection", MessageBoxButton.YesNo);
 
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        VolunteerWindow managerWindow = new VolunteerWindow(Id);
+                        managerWindow.Show();
+                    }
+                    else if (result == MessageBoxResult.No)
+                    {
+                        VolunteerListWindow managerVolunteerListWindow = new VolunteerListWindow();
+                        managerVolunteerListWindow.Show();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox)
+            {
+                Password = passwordBox.Password;
+            }
+        }
 
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Password))
+            {
+                // אפשר להוסיף כאן לוגיקה כדי להחזרת ערך ברירת מחדל או שינוי צבע
+            }
+        }
+
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // אפשר להוסיף כאן לוגיקה כדי להסיר ערך ברירת מחדל אם יש
+        }
+
+        private void IdTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Id == 0)
+            {
+                Id = -1;
+            }
+        }
+
+        private void IdTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Id == -1)
+            {
+                Id = 0;
+            }
+        }
     }
 }
